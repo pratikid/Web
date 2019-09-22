f2fd7cc494777641ce7574c066c67b05a00e2003
Pratik Khanolkar <pratik.khanolkar@atos.net>
2019-09-22 20:05:38 +0530

    Added New Files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using redis_sample.Models;
+
+namespace redis_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string CacheKey = "ASP_APP1";
+        private readonly IDistributedCache _distributedCache;
+
+        public HomeController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public async Task<IActionResult> Index(bool write = false)
+        {
+            if (write) {
+                await _distributedCache.SetStringAsync(CacheKey, DateTime.UtcNow.ToString());
+
+                ViewData["Message"] = "Write Cache";
+                return View();
+            }
+
+		    var value = await _distributedCache.GetStringAsync(CacheKey);
+            ViewData["Message"] = "Value Read: " + value;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack.Redis;
+
+namespace redis_sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDistributedRedisCache(option =>
+            {
+                option.Configuration = GetEnvironmentValue("redis:connection");
+                option.InstanceName = "master";
+            });
+            
+            services.AddScoped<IRedisClient>(s => new RedisClient(GetEnvironmentValue("redis:connection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private string GetEnvironmentValue(string key)
+        {
+            var value = Environment.GetEnvironmentVariable(key);
+
+            if (string.IsNullOrEmpty(value)) {
+                value = Configuration.GetValue<string>(key);
+            }
+
+            return value;
+        }
+    }
+}
